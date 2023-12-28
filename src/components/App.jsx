333a326746bdef8fb9bbd17707f67d5cc40e6142
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-12-28 21:33:25 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import React from 'react';
+
+import  FeedbackForm  from './FeedbackForm';
+import styles from './feedbackForm.module.css';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 20,
+        // color: '#010101'
       }}
     >
-      React homework template
+      <FeedbackForm />
     </div>
   );
 };
---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import './feedbackForm.module.css';
+import styles from './feedbackForm.module.css';
+
+class FeedbackForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  }
+
+  handleFeedback = type => {
+    this.setState(prevState => ({
+      ...prevState,
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h2 className={styles.title}>Please leave feedback</h2>
+        <button
+          className={styles.btn}
+          type="button"
+          onClick={() => this.handleFeedback('good')}
+        >
+          Good
+        </button>
+        <button
+          className={styles.btn}
+          type="button"
+          onClick={() => this.handleFeedback('neutral')}
+        >
+          Neutral
+        </button>
+        <button
+          className={styles.btn}
+          type="button"
+          onClick={() => this.handleFeedback('bad')}
+        >
+          Bad
+        </button>
+        <h2 className={styles.title}>Statistics</h2>
+      
+          <p className={styles.text}>
+            Good: <span>{this.state.good}</span>
+          </p>
+        
+          <p className={styles.text}>
+            Neutral: <span>{this.state.neutral}</span>
+          </p>
+       
+          <p className={styles.text}>
+            Bad: <span>{this.state.bad}</span>
+          </p>
+        
+      </div>
+    );
+  }
+}
+
+export default FeedbackForm;
