ef758f148f7c3b637be80134ae6eaf429ee039c9
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-12-30 09:01:42 +0200

    Update FeedbackForm.jsx

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,24 +4,23 @@ import './feedbackForm.module.css';
 import styles from './feedbackForm.module.css';
 
 class FeedbackForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  
+    state = {
       good: 0,
       neutral: 0,
       bad: 0,
     };
-  }
+  
 
-  handleFeedback(type) {
-    this.setState(prevState => {
-      const newState = { ...prevState };
-      newState[type] = prevState[type] + 1;
-      return newState;
-    });
-  }
+  handleFeedback = type => {
+    this.setState(prevState => ({
+      ...prevState,
+      [type]: prevState[type] + 1,
+    }));
+  };
 
   render() {
+    console.log(this.state)
     return (
       <div>
         <h2 className={styles.title}>Please leave feedback</h2>
@@ -47,18 +46,19 @@ class FeedbackForm extends Component {
           Bad
         </button>
         <h2 className={styles.title}>Statistics</h2>
-
-        <p className={styles.text}>
-          Good: <span>{this.state.good}</span>
-        </p>
-
-        <p className={styles.text}>
-          Neutral: <span>{this.state.neutral}</span>
-        </p>
-
-        <p className={styles.text}>
-          Bad: <span>{this.state.bad}</span>
-        </p>
+      
+          <p className={styles.text}>
+            Good: <span>{this.state.good}</span>
+          </p>
+        
+          <p className={styles.text}>
+            Neutral: <span>{this.state.neutral}</span>
+          </p>
+       
+          <p className={styles.text}>
+            Bad: <span>{this.state.bad}</span>
+          </p>
+        
       </div>
     );
   }
