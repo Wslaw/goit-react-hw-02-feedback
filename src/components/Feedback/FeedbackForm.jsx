9bb69b8176b9c2ec7a6d0b260403aef530dba6d6
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-05 23:17:55 +0200

    Update 2-2

--- src/components/Feedback/FeedbackForm.jsx
+++ src/components/Feedback/FeedbackForm.jsx
@@ -1,6 +1,4 @@
-// import { Component } from 'react';
-import React, { Component } from 'react';
-import './feedbackForm.module.css';
+import { Component } from 'react';
 import styles from './feedbackForm.module.css';
 
 class FeedbackForm extends Component {
@@ -10,48 +8,74 @@ class FeedbackForm extends Component {
     bad: 0,
   };
 
-  handleFeedback = type => {
+  static feedbackFormOptions = ['good', 'neutral', 'bad'];
+
+  handleFeedback(type) {
     this.setState(prevState => ({
       [type]: prevState[type] + 1,
     }));
-  };
+  }
 
-  feedbackElements(type, isButton) {
-    const feedbackTypes = Object.keys(this.state);
-    return feedbackTypes.map(type =>
-      isButton ? (
-        <button
-          key={type}
-          className={styles.btn}
-          type="button"
-          onClick={() => this.handleFeedback(type)}
-        >
-          {type.charAt(0).toUpperCase() + type.slice(1)}
-        </button>
-      ) : (
-        <p key={type} className={styles.text}>
-          {type.charAt(0).toUpperCase() + type.slice(1)}:{' '}
-          <span>{this.state[type]}</span>
-        </p>
-      )
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    const total = Object.values(this.state).reduce(
+      (accum, value) => accum + value,
+      0
     );
+    return total;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const total = this.countTotalFeedback();
+    if (!total) {
+      return 0;
+    }
+    const positivePercentage = Number(
+      ((this.state.good / this.countTotalFeedback()) * 100).toFixed(2)
+    );
+    return positivePercentage;
   }
 
   render() {
-    const feedbackTypes = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+
+    const buttonElements = FeedbackForm.feedbackFormOptions.map(name => (
+      <button
+        onClick={() => this.handleFeedback(name)}
+        key={name}
+        type="button"
+        className={styles.btn}
+      >
+        {name.charAt(0).toUpperCase() + name.slice(1)}
+      </button>
+    ));
+
+    const textElements = FeedbackForm.feedbackFormOptions.map(name => (
+      <p key={name} className={styles.text}>
+        {name.charAt(0).toUpperCase() + name.slice(1)}:{' '}
+        <span>{this.state[name]}</span>
+      </p>
+    ));
 
     return (
-      <div>
-        <h2 className={styles.title}>Please leave feedback</h2>
+      <div className={styles.wrapper}>
+        <div className={styles.action}>
+          <h2 className={styles.title}>Please leave feedback</h2>
 
-        {this.feedbackElements(feedbackTypes, true)}
+          {buttonElements}
+        </div>
+        <div className={styles.result}>
+          <h2 className={styles.title}>Statisics</h2>
 
-        <h2 className={styles.title}>Statistics</h2>
-
-        {this.feedbackElements(feedbackTypes, false)}
+          {textElements}
+          <p className={styles.text}>Total: {this.countTotalFeedback()}</p>
+          <p className={styles.text}>
+            Positive feedback percentage:{' '}
+            {this.countPositiveFeedbackPercentage()}%
+          </p>
+        </div>
       </div>
     );
   }
 }
-
 export default FeedbackForm;
