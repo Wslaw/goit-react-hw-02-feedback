87473e5f21d96808046f7fa94c9e6e25880bab94
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-06 11:00:11 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
+import { Component } from 'react';
 
 import  FeedbackForm  from '../components/Feedback/FeedbackForm';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 20,
-        // color: '#010101'
-      }}
-    >
-      <FeedbackForm />
-    </div>
-  );
-};
+
+
+class App extends Component {
+  state = {
+    good: 0,
+    netural: 0,
+    bad: 0,
+  };
+
+  render() {
+    
+    return {
+
+    }
+  }
+}
+
+
+export default App;
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 20,
+//         // color: '#010101'
+//       }}
+//     >
+//       <FeedbackForm />
+//     </div>
+//   );
+// };
